7ff01e4de6171b8ecd264facb395699bde6b65be
Antía Roig Regueira <roiregant@IESPIRINGALLA.AD>
2023-01-17 19:56:28 +0100

    Creación da estrutura base de Main

---
+++ Main.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class MainProgram {
+
+    static void Main(string[] args) {
+        Console.Write("Ola mundo!");
+    }
+}
+
+public abstract class Unidade {
+
+}
+
+public abstract class Aldean : Unidade {
+
+}
+
+public abstract class Guerriero : Unidade {
+
+}
+
+public abstract class Arqueiro : Unidade {
+
+}
