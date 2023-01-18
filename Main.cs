2c23d9b4f8023113a00892d1520d453eef456ee7
Jonatan Troyano <leonidasnegro3@gmail.com>
2023-01-18 18:52:22 +0100

    Listas creadas e unidades añadidas

--- Main.cs
+++ Main.cs
@@ -1,8 +1,41 @@
 using System;
+using System.Collections.Generic;
+
+/*
+ * Author : Jonatan Gonzalez Troyano
+ * 
+ * Fecha : 17/01/2023
+ * 
+ * Email : jonatan.zakarot@gmail.com
+ * 
+ * Github user : Gontrojon
+ * 
+ * */
 
 public class MainProgram {
 
+    // Creacion das listas
+    private static List<Unidade> equipoRoxo;
+    private static List<Unidade> equipoAzul;
+
     static void Main(string[] args) {
+        // inicializacion das listas
+        equipoRoxo = new List<Unidade>();
+        equipoAzul = new List<Unidade>();
+
+        // Añadese a cada equipo as seguintes unidades: 2 Aldeans 0 de ataque, 1 Gerreiro 10 de ataque, 1 Arqueiro 5 de ataque.
+
+        equipoRoxo.Add(new Aldean(0));
+        equipoRoxo.Add(new Aldean(0));
+        equipoRoxo.Add(new Guerreiro(10));
+        equipoRoxo.Add(new Arqueiro(5));
+
+        equipoAzul.Add(new Aldean(0));
+        equipoAzul.Add(new Aldean(0));
+        equipoAzul.Add(new Guerreiro(10));
+        equipoAzul.Add(new Arqueiro(5));
+
+        // Una vez implementado o bucle de xogo esta liña mostrara a info das unidades
         Console.Write("Ola mundo!");
     }
 }
@@ -28,9 +61,9 @@ public class Aldean : Unidade {
 
 }
 
-public class Guerriero : Unidade {
+public class Guerreiro : Unidade {
 
-    public Guerriero(int ataque) : base(ataque) { }
+    public Guerreiro(int ataque) : base(ataque) { }
 
 }
 
